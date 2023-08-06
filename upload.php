525aa351e4d7858f6774fb6d0f935160a1d83667
ar5entum <astitvajaiswalreturns@gmail.com>
2023-08-07 00:37:31 +0530

    file upload

--- upload.php
+++ upload.php
@@ -5,7 +5,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
   $uploadOk = true;
   $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-  // Check if the image file is an actual image or a fake image
   if (isset($_POST['submit'])) {
     $check = getimagesize($_FILES['file']['tmp_name']);
     if (!$check) {
@@ -14,25 +13,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     }
   }
 
-  // Check if the file already exists
   if (file_exists($targetFile)) {
     echo 'File already exists.';
     $uploadOk = false;
   }
 
-  // Check file size
   if ($_FILES['file']['size'] > 500000) {
     echo 'File is too large.';
     $uploadOk = false;
   }
 
-  // Allow only specific image file formats
   if ($imageFileType !== 'jpg' && $imageFileType !== 'png' && $imageFileType !== 'jpeg' && $imageFileType !== 'gif') {
     echo 'Only JPG, JPEG, PNG, and GIF files are allowed.';
     $uploadOk = false;
   }
 
-  // If upload is OK, move the file to the specified directory
   if ($uploadOk) {
     if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)) {
       echo 'File uploaded successfully.';
