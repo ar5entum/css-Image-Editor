5514ef7fae555408644bb3e7c07d7ee6ffdda2e0
ar5entum <astitvajaiswalreturns@gmail.com>
2023-08-07 10:00:17 +0530

    file upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# CSS Image Editor
+
+This application uses HTML CSS JS and PHP for simple image editing tasks. The purpose of this project is to be used as a tool for real time testing of image detection algorithms in one of my other projects.
+
+![Screen](screen.png "A Snapshot")
--- upload.php
+++ upload.php
@@ -1,39 +1,17 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
-  $targetDir = 'images/';
-  $targetFile = $targetDir . basename($_FILES['file']['name']);
-  $uploadOk = true;
-  $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-
-  if (isset($_POST['submit'])) {
-    $check = getimagesize($_FILES['file']['tmp_name']);
-    if (!$check) {
-      echo 'File is not an image.';
-      $uploadOk = false;
-    }
-  }
-
-  if (file_exists($targetFile)) {
-    echo 'File already exists.';
-    $uploadOk = false;
-  }
-
-  if ($_FILES['file']['size'] > 500000) {
-    echo 'File is too large.';
-    $uploadOk = false;
-  }
-
-  if ($imageFileType !== 'jpg' && $imageFileType !== 'png' && $imageFileType !== 'jpeg' && $imageFileType !== 'gif') {
-    echo 'Only JPG, JPEG, PNG, and GIF files are allowed.';
-    $uploadOk = false;
-  }
-
-  if ($uploadOk) {
-    if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)) {
-      echo 'File uploaded successfully.';
-    } else {
-      echo 'Error uploading the file.';
-    }
+$target_dir = "images/";
+$target_file = $target_dir . basename($_FILES["fileInput"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileInput"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
   }
 }
 ?>
